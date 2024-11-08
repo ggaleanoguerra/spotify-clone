9ad606b96717e72ed3e514caf3cb5853772d01b5
ggaleanoguerra <ggaleanoguerra@correo.unicordoba.edu.co>
2024-11-08 11:27:36 -0500

    Add dotenv support and update layout styles
    - Integrated dotenv for environment variable management.
    - Updated AsideMenu component layout and styles.
    - Enhanced icon SVGs with fill color for better visibility.
    - Improved global styles with new font faces and background color.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 // https://astro.build/config
 export default defineConfig({
