2b14035fb70c087ff0cf48344f87857480628126
ggaleanoguerra <ggaleanoguerra@correo.unicordoba.edu.co>
2024-11-17 15:57:06 -0500

    Add React integration and update TypeScript configuration for JSX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 import dotenv from 'dotenv';
 
+import react from '@astrojs/react';
+
 dotenv.config();
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: "server",  
   adapter: node({ mode: 'standalone' }),
   vite: {
